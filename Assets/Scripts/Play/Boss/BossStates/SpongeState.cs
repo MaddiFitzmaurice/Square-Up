3645794db865393b31b662f66212c9ab82ff5f15
Maddi <70463961+Madfizz@users.noreply.github.com>
2021-09-09 18:23:52 +1000

    Start of Boss States
    - Set up boss states

---
+++ Assets/Scripts/Play/Boss/BossStates/SpongeState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpongeState : BaseState
+{
+    public override void Enter()
+    {
+        
+    }
+
+    public override void LogicUpdate()
+    {
+        
+    }
+
+    public override void PhysicsUpdate()
+    {
+        
+    }
+}
